a4b2134dd68065dede2bb67072c808d660a80d6d
Joshua T Corbin <jcorbin@wunjo.org>
2017-11-13 23:07:47 -0800

    Add point.Box

---
+++ internal/point/box.go
@@ -0,0 +1,33 @@
+package point
+
+// Box represents a bounding box defined by a top-left and bottom-right point.
+type Box struct {
+	TopLeft     Point
+	BottomRight Point
+}
+
+// Size returns the width and height of the box as a point.
+func (b Box) Size() Point {
+	pt := b.BottomRight.Sub(b.TopLeft).Abs()
+	pt.X++
+	pt.Y++
+	return pt
+}
+
+// ExpandTo expands a coy of the box to include the given point, returning the
+// copy.
+func (b Box) ExpandTo(pt Point) Box {
+	if pt.X < b.TopLeft.X {
+		b.TopLeft.X = pt.X
+	}
+	if pt.Y < b.TopLeft.Y {
+		b.TopLeft.Y = pt.Y
+	}
+	if pt.X > b.BottomRight.X {
+		b.BottomRight.X = pt.X
+	}
+	if pt.Y > b.BottomRight.Y {
+		b.BottomRight.Y = pt.Y
+	}
+	return b
+}
