7e04fa8a787219a729840c548389c7ac9814d538
Joshua T Corbin <jcorbin@wunjo.org>
2017-11-15 08:27:17 -0800

    ecs: add a skeleton main.go

---
+++ internal/ecs/skeleton/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"log"
+
+	"github.com/jcorbin/execs/internal/ecs"
+	"github.com/jcorbin/execs/internal/point"
+	"github.com/jcorbin/execs/internal/view"
+)
+
+// const (
+// 	XXX ecs.ComponentType = 1 << iota
+// )
+
+// const (
+// 	XXX = YYY | ZZZ
+// )
+
+type world struct {
+	View *view.View
+	ecs.Core
+	// XXX
+}
+
+func createWorld(v *View) *world {
+	w := world{
+		View: v,
+	}
+
+	// TODO: something interesting
+
+	return w
+}
+
+func (w *world) Render(grid view.Grid, avail point.Point) view.Grid {
+	// TODO: translate world state into a grid, return it
+
+}
+
+func (w *world) Step() bool {
+	select {
+	case k := <-v.Keys():
+		// TODO: do something in response to the key
+
+	case <-v.Done():
+		return false
+	}
+
+	return true
+}
+
+func main() {
+	if err := view.JustKeepRunning(func(v *view.View) (view.Stepable, error) {
+		return createWorld(v), nil
+	}); err != nil {
+		log.Fatal(err)
+	}
+}
