28ec829ecee207b7cfd2e6feb1c344f492b81e55
Joshua T Corbin <jcorbin@wunjo.org>
2017-11-16 16:47:52 -0800

    internal/ecs: unify and generalize iteration

--- internal/ecs/core.go
+++ internal/ecs/core.go
@@ -44,48 +44,49 @@ func (core Core) CountAny(mask ComponentType) int {
 	return n
 }
 
-// IterAll returns an iterator over entities with all of the given components.
-func (core Core) IterAll(mask ComponentType) Iterator {
+// Iter returns an iterator that will iterate over all entities with type t
+// where t.All(all) is true if all is not 0 and t.Any(any) is true if any is
+// not 0.  all of the given components.
+func (core Core) Iter(all, any ComponentType) Iterator {
 	return Iterator{
-		ents:   core.Entities,
-		filter: allTypeFilter(mask),
+		ents: core.Entities,
+		all:  all,
+		any:  any,
 	}
 }
+
+// IterAll returns an iterator over entities with all of the given components.
+func (core Core) IterAll(mask ComponentType) Iterator { return core.Iter(mask, 0) }
 
 // IterAny returns an iterator over entities with any of the given components.
-func (core Core) IterAny(mask ComponentType) Iterator {
-	return Iterator{
-		ents:   core.Entities,
-		filter: anyTypeFilter(mask),
-	}
-}
-
-type typeFilter interface {
-	test(ComponentType) bool
-}
-
-type allTypeFilter ComponentType
-type anyTypeFilter ComponentType
-
-func (t allTypeFilter) test(ot ComponentType) bool { return ot&ComponentType(t) == ComponentType(t) }
-func (t anyTypeFilter) test(ot ComponentType) bool { return ot&ComponentType(t) != 0 }
+func (core Core) IterAny(mask ComponentType) Iterator { return core.Iter(0, mask) }
 
 // Iterator iterates over entities of specific type(s) within a Core.
 type Iterator struct {
-	ents   []ComponentType
-	i      int
-	filter typeFilter
+	ents     []ComponentType
+	i        int
+	all, any ComponentType
 }
 
 // Reset resets the iterator, causing it to iterates over again.
 func (it *Iterator) Reset() { it.i = 0 }
 
+func (it Iterator) test(t ComponentType) bool {
+	if it.all != 0 && !t.All(it.all) {
+		return false
+	}
+	if it.any != 0 && !t.Any(it.any) {
+		return false
+	}
+	return true
+}
+
 // Count returns a count of how many entities are yet to come.
 func (it *Iterator) Count() int {
-	ents, test := it.ents, it.filter.test
+	ents := it.ents
 	n := 0
 	for i := it.i; i < len(ents); i++ {
-		if test(ents[i]) {
+		if it.test(ents[i]) {
 			n++
 		}
 	}
@@ -96,9 +97,9 @@ func (it *Iterator) Count() int {
 // entities yet to iterate; otherwise false is return with a 0 id and
 // ComponentNone type.
 func (it *Iterator) Next() (int, ComponentType, bool) {
-	ents, test, i := it.ents, it.filter.test, it.i
+	ents, i := it.ents, it.i
 	for i < len(ents) {
-		if test(ents[i]) {
+		if it.test(ents[i]) {
 			it.i = i + 1
 			return i, ents[i], true
 		}
