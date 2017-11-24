e3f8f65c68de19beed58a07c68dd3f7e342590fa
Joshua T Corbin <jcorbin@wunjo.org>
2017-11-24 13:59:39 -0800

    internal/ecs: update skeleton for new HUD-based view

--- internal/ecs/skeleton/main.go
+++ internal/ecs/skeleton/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/jcorbin/execs/internal/ecs"
@@ -17,13 +18,33 @@ import (
 
 type world struct {
 	ecs.Core
+	logs []string
 
 	// TODO: your state here
 }
 
-func (w *world) Render(ctx *view.Context) error {
-	// TODO: translate world state into ctx
+func (w *world) Render(termGrid view.Grid) error {
+	hud := view.HUD{
+		// World: , // TODO: render your world grid and pass it here
+		Logs: w.logs,
+	}
 
+	// TODO: call hud methods to build a basic UI, e.g.:
+	hud.AddHeaderF("<left1")
+	hud.AddHeaderF("<left2")
+	hud.AddHeaderF(">right1")
+	hud.AddHeaderF(">right2")
+	hud.AddHeaderF("center by default")
+
+	hud.AddFooterF("footer has the same stuff")
+	hud.AddFooterF(">one")
+	hud.AddFooterF(">two")
+	hud.AddFooterF(".>three") // the "." forces a new line
+
+	// NOTE: more advanced UI components may use:
+	// hud.AddRenderable(ren view.Renderable, align view.Align)
+
+	hud.Render(termGrid)
 	return nil
 }
 
@@ -33,15 +54,21 @@ func (w *world) Close() error {
 	return nil
 }
 
-func (w *world) HandleKey(v *view.View, k view.KeyEvent) error {
+func (w *world) HandleKey(k view.KeyEvent) error {
 	// TODO: do something with it
 
 	return nil
 }
 
+func (w *world) log(mess string, args ...interface{}) {
+	w.logs = append(w.logs, fmt.Sprintf(mess, args...))
+}
+
 func main() {
 	if err := view.JustKeepRunning(func(v *view.View) (view.Client, error) {
 		var w world
+
+		w.log("Hello World Of Democraft!")
 
 		// TODO: something interesting
 
