7fcf7991996d98cd112bb46cfb9945e3f1a08c7c
Joshua T Corbin <jcorbin@wunjo.org>
2017-11-28 08:31:31 -0800

    internal/perf: start a perf profiler

---
+++ internal/perf/perf.go
@@ -0,0 +1,172 @@
+package perf
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"runtime"
+	"runtime/pprof"
+	"syscall"
+	"time"
+
+	"github.com/jcorbin/execs/internal/ecs"
+)
+
+const (
+	numSamples = 64
+)
+
+// Perf is an ecs.Proc that collects Perf data.
+type Perf struct {
+	ecs.Proc
+
+	outputBase    string
+	shouldProfile bool
+	profiling     bool
+	err           error
+	cpuProfF      *os.File
+	profDebug     int
+
+	round    int
+	i        int
+	time     [numSamples]struct{ start, end time.Time }
+	memStats [numSamples]runtime.MemStats
+}
+
+// Init sets up the perf system, writing results into the given directory.
+func (perf *Perf) Init(outputBase string, proc ecs.Proc) {
+	perf.profDebug = 2
+	perf.outputBase = outputBase
+	perf.Proc = proc
+}
+
+// Process runs a round of the perf system.
+func (perf *Perf) Process() {
+	perf.round++
+
+	if err := perf.maybeProfile(); err != nil {
+		perf.err = err
+		_ = perf.stopProfiling()
+	}
+
+	perf.time[perf.i].start = time.Now()
+	perf.Proc.Process()
+	perf.time[perf.i].end = time.Now()
+
+	runtime.ReadMemStats(&perf.memStats[perf.i])
+
+	if perf.profiling {
+		if err := perf.takeProfile(); err != nil {
+			perf.err = err
+			_ = perf.stopProfiling()
+		}
+	}
+
+	perf.i = (perf.i + 1) % numSamples
+}
+
+// Start requests profiling to start, this happens during the next Process
+// round.
+func (perf *Perf) Start() { perf.shouldProfile = true }
+
+// Stop requests profiling to start, this happens during the next Process
+// round.
+func (perf *Perf) Stop() { perf.shouldProfile = false }
+
+// Close cleans up the profiler, returning any error.
+func (perf *Perf) Close() error {
+	if serr := perf.stopProfiling(); perf.err == nil {
+		perf.err = serr
+	}
+	return perf.err
+}
+
+// Err return any profiling error encountered; if this is non-nil, then
+// profiling will not start.
+func (perf *Perf) Err() error { return perf.err }
+
+// Running returns whether profiling has been requested, and whether it
+// actually active.
+func (perf *Perf) Running() (should, are bool) {
+	return perf.shouldProfile,
+		perf.profiling
+}
+
+func (perf *Perf) maybeProfile() error {
+	if perf.err != nil {
+		return perf.err
+	} else if perf.profiling && !perf.shouldProfile {
+		return perf.stopProfiling()
+	} else if !perf.profiling && perf.shouldProfile {
+		return perf.startProfiling()
+	}
+	return nil
+}
+
+func (perf *Perf) startProfiling() error {
+	if perf.profiling {
+		return nil
+	}
+	f, err := perf.createOutput("cpu")
+	if err != nil {
+		return err
+	}
+	if err := pprof.StartCPUProfile(f); err != nil {
+		_ = f.Close()
+		return err
+	}
+	perf.cpuProfF = f
+	perf.profiling = true
+	return perf.takeProfile()
+}
+
+func (perf *Perf) stopProfiling() (err error) {
+	if perf.cpuProfF != nil {
+		pprof.StopCPUProfile()
+		err = perf.cpuProfF.Close()
+		perf.cpuProfF = nil
+		if err != nil {
+			err = fmt.Errorf("failed to close \"cpu\" output file: %v", err)
+		}
+	}
+	perf.shouldProfile = false
+	perf.profiling = false
+	return nil
+}
+
+func (perf *Perf) takeProfile() error {
+	for _, prof := range pprof.Profiles() {
+		f, err := perf.createOutput(prof.Name())
+		if err != nil {
+			return err
+		}
+		err = prof.WriteTo(f, perf.profDebug)
+		if cerr := f.Close(); err == nil {
+			err = cerr
+		}
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (perf *Perf) createOutput(name string) (*os.File, error) {
+	pth := path.Join(perf.outputBase, fmt.Sprintf("t%d", perf.round), name)
+	f, err := createMkdirAll(pth)
+	if err != nil {
+		err = fmt.Errorf("failed to create %q output file: %v", name, err)
+	}
+	return f, err
+}
+
+func createMkdirAll(name string) (*os.File, error) {
+	f, err := os.Create(name)
+	if pe, ok := err.(*os.PathError); ok && pe.Err == syscall.ENOENT {
+		err = os.MkdirAll(path.Dir(name), 0777)
+		if err == nil {
+			return os.Create(name)
+		}
+	}
+	return f, err
+}
