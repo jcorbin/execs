55aa975e339e56a85f64e7063aa6ebd96b28d3f6
Joshua T Corbin <jcorbin@wunjo.org>
2018-08-03 10:28:03 -0700

    WIP

---
+++ main.go
@@ -0,0 +1,335 @@
+package main
+
+import (
+	"image"
+	"log"
+
+	termbox "github.com/nsf/termbox-go"
+
+	"github.com/jcorbin/execs/internal/ecs"
+)
+
+var (
+	aiColors    = []termbox.Attribute{124, 160, 196, 202, 208, 214}
+	soulColors  = []termbox.Attribute{19, 20, 21, 27, 33, 39}
+	itemColors  = []termbox.Attribute{22, 23, 29, 35, 41, 47}
+	wallColors  = []termbox.Attribute{233, 234, 235, 236, 237, 238, 239}
+	floorColors = []termbox.Attribute{232, 233, 234}
+)
+
+type game struct {
+	world world
+	name  []byte
+}
+
+type world struct {
+	ecs.Scope
+
+	char []rune
+	attr []termbox.Attribute
+	pos  []image.Point
+}
+
+type imid int
+
+type imtui struct {
+	ev   termbox.Event
+	size image.Point
+
+	next   imid
+	active imid
+}
+
+func (it *imtui) nextID() imid {
+	id := it.next
+	it.next++
+	return id
+}
+
+func (it *imtui) do(ev *termbox.Event, draw func(*imtui)) {
+	const coldef = termbox.ColorDefault
+
+	if ev == nil {
+		it.ev = termbox.Event{
+			// NOTE not the zero value, so we have to specify it
+			Type: termbox.EventNone,
+		}
+	} else {
+		it.ev = *ev
+	}
+
+	it.next = 1
+
+	termbox.Clear(coldef, coldef)
+	defer termbox.Flush()
+
+	it.size.X, it.size.Y = termbox.Size()
+	draw(it)
+}
+
+func (it *imtui) keyPressed() (termbox.Key, rune, bool) {
+	if it.ev.Type != termbox.EventKey {
+		return 0, 0, false
+	}
+	return it.ev.Key, it.ev.Ch, true
+}
+
+/*
+
+// Draws the EditBox in the given location, 'h' is not used at the moment
+func (eb *EditBox) Draw(x, y, w, h int) {
+	eb.AdjustVOffset(w)
+
+	const coldef = termbox.ColorDefault
+	fill(x, y, w, h, termbox.Cell{Ch: ' '})
+
+	t := eb.text
+	lx := 0
+	tabstop := 0
+	for {
+		rx := lx - eb.line_voffset
+		if len(t) == 0 {
+			break
+		}
+
+		if lx == tabstop {
+			tabstop += tabstop_length
+		}
+
+		if rx >= w {
+			termbox.SetCell(x+w-1, y, '→',
+				coldef, coldef)
+			break
+		}
+
+		r, size := utf8.DecodeRune(t)
+		if r == '\t' {
+			for ; lx < tabstop; lx++ {
+				rx = lx - eb.line_voffset
+				if rx >= w {
+					goto next
+				}
+
+				if rx >= 0 {
+					termbox.SetCell(x+rx, y, ' ', coldef, coldef)
+				}
+			}
+		} else {
+			if rx >= 0 {
+				termbox.SetCell(x+rx, y, r, coldef, coldef)
+			}
+			lx += runewidth.RuneWidth(r)
+		}
+	next:
+		t = t[size:]
+	}
+
+	if eb.line_voffset != 0 {
+		termbox.SetCell(x, y, '←', coldef, coldef)
+	}
+}
+
+// Adjusts line visual offset to a proper value depending on width
+func (eb *EditBox) AdjustVOffset(width int) {
+	ht := preferred_horizontal_threshold
+	max_h_threshold := (width - 1) / 2
+	if ht > max_h_threshold {
+		ht = max_h_threshold
+	}
+
+	threshold := width - 1
+	if eb.line_voffset != 0 {
+		threshold = width - ht
+	}
+	if eb.cursor_voffset-eb.line_voffset >= threshold {
+		eb.line_voffset = eb.cursor_voffset + (ht - width + 1)
+	}
+
+	if eb.line_voffset != 0 && eb.cursor_voffset-eb.line_voffset < ht {
+		eb.line_voffset = eb.cursor_voffset - ht
+		if eb.line_voffset < 0 {
+			eb.line_voffset = 0
+		}
+	}
+}
+
+func (eb *EditBox) MoveCursorTo(boffset int) {
+	eb.cursor_boffset = boffset
+	eb.cursor_voffset, eb.cursor_coffset = voffset_coffset(eb.text, boffset)
+}
+
+func (eb *EditBox) RuneUnderCursor() (rune, int) {
+	return utf8.DecodeRune(eb.text[eb.cursor_boffset:])
+}
+
+func (eb *EditBox) RuneBeforeCursor() (rune, int) {
+	return utf8.DecodeLastRune(eb.text[:eb.cursor_boffset])
+}
+
+func (eb *EditBox) MoveCursorOneRuneBackward() {
+	if eb.cursor_boffset == 0 {
+		return
+	}
+	_, size := eb.RuneBeforeCursor()
+	eb.MoveCursorTo(eb.cursor_boffset - size)
+}
+
+func (eb *EditBox) MoveCursorOneRuneForward() {
+	if eb.cursor_boffset == len(eb.text) {
+		return
+	}
+	_, size := eb.RuneUnderCursor()
+	eb.MoveCursorTo(eb.cursor_boffset + size)
+}
+
+func (eb *EditBox) MoveCursorToBeginningOfTheLine() {
+	eb.MoveCursorTo(0)
+}
+
+func (eb *EditBox) MoveCursorToEndOfTheLine() {
+	eb.MoveCursorTo(len(eb.text))
+}
+
+func (eb *EditBox) DeleteRuneBackward() {
+	if eb.cursor_boffset == 0 {
+		return
+	}
+
+	eb.MoveCursorOneRuneBackward()
+	_, size := eb.RuneUnderCursor()
+	eb.text = byte_slice_remove(eb.text, eb.cursor_boffset, eb.cursor_boffset+size)
+}
+
+func (eb *EditBox) DeleteRuneForward() {
+	if eb.cursor_boffset == len(eb.text) {
+		return
+	}
+	_, size := eb.RuneUnderCursor()
+	eb.text = byte_slice_remove(eb.text, eb.cursor_boffset, eb.cursor_boffset+size)
+}
+
+func (eb *EditBox) DeleteTheRestOfTheLine() {
+	eb.text = eb.text[:eb.cursor_boffset]
+}
+
+func (eb *EditBox) InsertRune(r rune) {
+	var buf [utf8.UTFMax]byte
+	n := utf8.EncodeRune(buf[:], r)
+	eb.text = byte_slice_insert(eb.text, eb.cursor_boffset, buf[:n])
+	eb.MoveCursorOneRuneForward()
+}
+
+// Please, keep in mind that cursor depends on the value of line_voffset, which
+// is being set on Draw() call, so.. call this method after Draw() one.
+func (eb *EditBox) CursorX() int {
+	return eb.cursor_voffset - eb.line_voffset
+}
+*/
+
+func (it *imtui) doTextEdit(s []byte) (_ []byte, changed, submitted bool) {
+	id := it.nextID()
+	if it.active == 0 {
+		// TODO better focus system
+		if ke, _, ok := it.keyPressed(); ok {
+			if ke == termbox.KeyEnter {
+				it.active = id
+			}
+		}
+	}
+	if id != it.active {
+		// TODO draw inactive
+		return nil, false, false
+	}
+
+	// TODO viva la state
+	// type EditBox struct {
+	// 	text           []byte
+	// 	line_voffset   int
+	// 	cursor_boffset int // cursor offset in bytes
+	// 	cursor_voffset int // visual cursor offset in termbox cells
+	// 	cursor_coffset int // cursor offset in unicode code points
+	// }
+
+	// if ke, ch, ok := it.keyPressed(); ok {
+	// 	switch ke {
+	// 	// case termbox.KeyArrowLeft, termbox.KeyCtrlB:
+	// 	// edit_box.MoveCursorOneRuneBackward()
+	// 	// case termbox.KeyArrowRight, termbox.KeyCtrlF:
+	// 	// edit_box.MoveCursorOneRuneForward()
+	// 	// case termbox.KeyBackspace, termbox.KeyBackspace2:
+	// 	// edit_box.DeleteRuneBackward()
+	// 	// case termbox.KeyDelete, termbox.KeyCtrlD:
+	// 	// edit_box.DeleteRuneForward()
+	// 	// case termbox.KeyTab:
+	// 	// edit_box.InsertRune('\t')
+	// 	// case termbox.KeySpace:
+	// 	// edit_box.InsertRune(' ')
+	// 	// case termbox.KeyCtrlK:
+	// 	// edit_box.DeleteTheRestOfTheLine()
+	// 	// case termbox.KeyHome, termbox.KeyCtrlA:
+	// 	// edit_box.MoveCursorToBeginningOfTheLine()
+	// 	// case termbox.KeyEnd, termbox.KeyCtrlE:
+	// 	// edit_box.MoveCursorToEndOfTheLine()
+	// 	// default:
+	// 	// if ch != 0 {
+	// 	// edit_box.InsertRune(ch)
+	// 	// }
+	// 	}
+	// }
+
+	// pos := it.size.Div(2)
+	// midy := h / 2
+	// midx := (w - edit_box_width) / 2
+
+	// termbox.SetCell(midx-1, midy, '│', coldef, coldef)
+	// termbox.SetCell(midx+edit_box_width, midy, '│', coldef, coldef)
+	// termbox.SetCell(midx-1, midy-1, '┌', coldef, coldef)
+	// termbox.SetCell(midx-1, midy+1, '└', coldef, coldef)
+	// termbox.SetCell(midx+edit_box_width, midy-1, '┐', coldef, coldef)
+	// termbox.SetCell(midx+edit_box_width, midy+1, '┘', coldef, coldef)
+	// fill(midx, midy-1, edit_box_width, 1, termbox.Cell{Ch: '─'})
+	// fill(midx, midy+1, edit_box_width, 1, termbox.Cell{Ch: '─'})
+
+	// edit_box.Draw(midx, midy, edit_box_width, 1)
+	// termbox.SetCursor(midx+edit_box.CursorX(), midy)
+
+	// tbprint(midx+6, midy+3, coldef, coldef, "Press ESC to quit")
+	return s, false, false
+}
+
+func (g *game) draw(it *imtui) {
+	if s, changed, submitted := it.doTextEdit(g.name); changed {
+		g.name = s
+	} else if submitted {
+		log.Printf("hello %q", s)
+		g.name = s[:0]
+	}
+}
+
+func run() error {
+	if err := termbox.Init(); err != nil {
+		return err
+	}
+	defer termbox.Close()
+	termbox.SetInputMode(termbox.InputEsc)
+	var (
+		g  game
+		it imtui
+	)
+	it.do(nil, g.draw)
+	for {
+		ev := termbox.PollEvent()
+		if ev.Type == termbox.EventKey && ev.Key == termbox.KeyEsc {
+			return nil
+		} else if ev.Type == termbox.EventError {
+			return ev.Err
+		}
+		it.do(&ev, g.draw)
+	}
+}
+
+func main() {
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
