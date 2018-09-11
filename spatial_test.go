25542a3bcac5ef05dc9327f9c3daa7a97c9d3d75
Joshua T Corbin <jcorbin@wunjo.org>
2018-09-10 18:36:15 -0700

    Test origin spatial misses

--- spatial_test.go
+++ spatial_test.go
@@ -10,12 +10,14 @@ import (
 
 func Test_position(t *testing.T) {
 	for _, tc := range []struct {
-		name string
-		data []image.Point
+		name  string
+		data  []image.Point
+		zeros []image.Point
 	}{
 		{
-			name: "origin",
-			data: []image.Point{image.Pt(0, 0)},
+			name:  "origin",
+			data:  []image.Point{image.Pt(0, 0)},
+			zeros: testPointRing(image.Rect(-1, -1, 1, 1)),
 		},
 		{
 			name: "four-quadrant square",
@@ -47,23 +49,13 @@ func Test_position(t *testing.T) {
 		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
-			var sc ecs.Scope
-			var pos position
-			sc.Watch(0, 0, &pos)
-
-			// load
-			for i, pt := range tc.data {
-				idt := ecs.Type(i + 1)
-				ent := sc.Create(idt)
-				posd := pos.Get(ent)
-				posd.SetPoint(pt)
-			}
+			tp := newTestPos(t, tc.data)
 
 			// readback via At
 			for i, pt := range tc.data {
 				idt := ecs.Type(i + 1)
 				found := false
-				for q := pos.At(pt); q.next(); {
+				for q := tp.pos.At(pt); q.next(); {
 					posd := q.handle()
 					ent := posd.Entity()
 					if ent.Type() == idt {
@@ -72,29 +64,80 @@ func Test_position(t *testing.T) {
 					}
 				}
 				if !assert.True(t, found, "expected to find [%v]%v", i, pt) {
-					t.Logf("q: %v", pos.At(pt))
-
-					t.Logf("id,type,pi,X,Y,key,ix")
-					for i := 0; i < pos.ArrayIndex.Len(); i++ {
-						id := pos.ArrayIndex.ID(i)
-						ent := sc.Entity(id)
-						t.Logf(
-							"%v,%v,%v,%v,%v,%016x,%v",
-							id, ent.Type(), i,
-							pos.pt[i].X, pos.pt[i].Y,
-							pos.qi.ks[i], pos.qi.ix[i],
-						)
-					}
-
+					t.Logf("q: %v", tp.pos.At(pt))
+					tp.dump()
 				}
 			}
 
-			// TODO At misses
+			// At zeros
+			for _, pt := range tc.zeros {
+				if q := tp.pos.At(pt); !assert.False(t, q.next(), "expected zero at %v", pt) {
+					t.Logf("q: %v", tp.pos.At(pt))
+					tp.dump()
+				}
+			}
 
 			// TODO Within queries
 		})
 	}
 
+}
+
+type testPos struct {
+	*testing.T
+	ecs.Scope
+	pos position
+}
+
+func newTestPos(t *testing.T, data []image.Point) *testPos {
+	var tp testPos
+	tp.T = t
+	tp.Watch(0, 0, &tp.pos)
+	for i, pt := range data {
+		tp.create(ecs.Type(i+1), pt)
+	}
+	return &tp
+}
+
+func (tp *testPos) create(t ecs.Type, pt image.Point) ecs.Entity {
+	ent := tp.Create(t)
+	posd := tp.pos.Get(ent)
+	posd.SetPoint(pt)
+	return ent
+}
+
+func (tp *testPos) dump() {
+	tp.Logf("id,type,pi,X,Y,key,ix")
+	for i := 0; i < tp.pos.ArrayIndex.Len(); i++ {
+		id := tp.pos.ArrayIndex.ID(i)
+		ent := tp.Entity(id)
+		tp.Logf(
+			"%v,%v,%v,%v,%v,%016x,%v",
+			id, ent.Type(), i,
+			tp.pos.pt[i].X, tp.pos.pt[i].Y,
+			tp.pos.qi.ks[i], tp.pos.qi.ix[i],
+		)
+	}
+}
+
+func testPointRing(r image.Rectangle) (pts []image.Point) {
+	pt := r.Min
+	pts = make([]image.Point, 0, 2*r.Dx()+2*r.Dy())
+	for _, st := range []struct {
+		d image.Point
+		n int
+	}{
+		{image.Pt(1, 0), r.Dx()},
+		{image.Pt(0, 1), r.Dy()},
+		{image.Pt(-1, 0), r.Dx()},
+		{image.Pt(0, -1), r.Dy()},
+	} {
+		for i := 0; i < st.n; i++ {
+			pts = append(pts, pt)
+			pt = pt.Add(st.d)
+		}
+	}
+	return pts
 }
 
 func testPointRect(r image.Rectangle) (pts []image.Point) {
